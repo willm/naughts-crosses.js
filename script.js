dba71b699ba0c935001c87fbf8c13399f66f16f8
Will Munn <willmunn@hotmail.com>
2011-08-25 00:57:17 +0100

    added raphael version

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 $(function(){
-now.colorSwitch = function changeColor(color,id){
-	console.log(color);
-	$('#'+id).css('background-color', color);
-}
-$('.box').click(function(evt){
-				console.log(evt.currentTarget.id);
-				now.clicked(evt.currentTarget.id);
-				});
+
+	var grid =[];	
+	
+	now.colorSwitch = function changeColor(color,id){
+		console.log(color);
+		$('#'+id).css('background-color', color);
+	};
+	$('.box').click(function(evt){
+					console.log(evt.currentTarget.id);
+					now.clicked(evt.currentTarget.id);
+					});	
+	
+	(function raphaelVersion (){
+		var width = 300,
+			height = 300,
+			rect,
+			paper = Raphael("raphael",width,height);
+			
+		for(var i =0; i< width; i+=100){
+			for(var j =0; j< height; j+=100){
+				rect = paper.rect(i, j,200,200);
+				rect.attr("stroke","black");
+				rect.node.onclick=function(){cross();};
+				grid.push(rect);
+				
+			}
+		}
+		console.log(grid[0]);
+		console.log(grid[4].attrs);
+	})();
 });
+	
+	function cross(){
+		  console.log("hi");
+	}
+
+
+
