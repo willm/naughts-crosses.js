eeb16cff8dd96f3edbcd8584f6c242fe0bfa8c7b
Will Munn <willmunn@hotmail.com>
2011-08-26 15:42:17 +0100

    formatting

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ $(function(){
 		for (var i =0; i<grid.length; i++) {
 		  if(grid[i].node.id === id){
 		  	console.log(grid[i].getBBox().x);
-		  	paper.text(grid[i].getBBox().x+50, grid[i].getBBox().y+50,type).attr({ "font-size": 100});
+		  	paper.text(
+				  		grid[i].getBBox().x+50,
+				  		grid[i].getBBox().y+50,
+				  		type
+		  			)
+		  			.attr({ "font-size": 100});
 		  };
 		}
 	};
@@ -24,6 +29,7 @@ $(function(){
 				rect = paper.rect(i, j,200,200);
 				rect.attr("stroke","black");
 				rect.attr("fill","white");
+				console.log(rect.id);
 				rect.node.id = i+":"+j;//unique id for each square
 				$(rect.node).click(function(evt){
 					console.log(evt.currentTarget.id);
